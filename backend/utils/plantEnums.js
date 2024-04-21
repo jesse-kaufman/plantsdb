b4be6c4f47cf2d252483a7f035b38b49ffccfedf
jesse-kaufman <jesse@jessekaufman.com>
2024-04-21 06:29:27 -0500

    Move enum functions to separate file

---
+++ backend/utils/plantEnums.js
@@ -0,0 +1,19 @@
+/**
+ * Gets list of possible plant statuses
+ *
+ * @returns {Array} List of valid plant statuses
+ */
+exports.getValidPlantStatuses = () => ["active", "inactive", "archived"];
+
+/**
+ * Gets list of possible plant sources
+ *
+ * @returns {Array} List of valid plant sources
+ */
+exports.getValidPlantSources = () => ["seed", "clone"];
+
+/**
+ * Returns an array of valid stages for a plant.
+ * @returns {Array} An array of valid stages: ["seedling", "veg", "flower", "cure"].
+ */
+exports.getValidPlantStages = () => ["seedling", "veg", "flower", "cure"];
