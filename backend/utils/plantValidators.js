2189bf9a90871521178205667a4fd046aa6d4d9e
jesse-kaufman <jesse@jessekaufman.com>
2024-04-24 08:18:55 -0500

    Simplify code

--- backend/utils/plantValidators.js
+++ backend/utils/plantValidators.js
@@ -1,30 +1,22 @@
 exports.validatePlantId = async (plantId) => {
-  try {
-    const count = await plantModel.countDocuments({
-      status: "active",
-      plantId: plantId.trim(),
-    });
-    if (count === 0) {
-      return true;
-    }
-  } catch (error) {
-    throw new Error(error.message);
+  const count = await plantModel.countDocuments({
+    status: "active",
+    plantId: plantId.trim(),
+  });
+  if (count === 0) {
+    return true;
   }
 
   return false;
 };
 
 exports.validatePlantName = async (plantName) => {
-  try {
-    const count = await plantModel.countDocuments({
-      status: "active",
-      name: name.trim(),
-    });
-    if (count === 0) {
-      return true;
-    }
-  } catch (error) {
-    throw new Error(error.message);
+  const count = await plantModel.countDocuments({
+    status: "active",
+    name: name.trim(),
+  });
+  if (count === 0) {
+    return true;
   }
 
   return false;
