5b0681c907f40c8a5957c42799897a30b0c6d62c
jesse-kaufman <jesse@jessekaufman.com>
2024-05-01 17:14:12 -0500

    Send 404 if plant not found

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -23,8 +23,11 @@ exports.getPlant = async (req, res) => {
     // Find the plant
     //
     const plant = await getPlantById(req.params.plantId, status);
-
-    res.status(200).json(plant);
+    if (plant) {
+      res.status(200).json(plant);
+    } else {
+      res.status(404).json({ error: "Plant not found" });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
     return;
