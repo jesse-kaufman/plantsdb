7aa3ee04e1c9037f35df4b5e9024c325d4cadca1
jesse-kaufman <jesse@jessekaufman.com>
2024-06-24 20:52:23 -0500

    Switch back to arrow function expressions and reorder

--- src/utils/plantChangeService.js
+++ src/utils/plantChangeService.js
@@ -1,32 +1,27 @@
 import dayjs from 'dayjs'
 
-const getStatusChangeMsg = (oldStatus, newStatus) => {
-  // Status did not change
-  if (oldStatus === newStatus) return null
-
-  // Status is archived
-  if (newStatus === 'archived') return 'Plant archived.'
-
-  // Status was archived and now is active
-  if (oldStatus === 'archived') return 'Plant unarchived.'
-
-  // Status was inactive and now is active
-  if (oldStatus === 'inactive') return 'Plant undeleted.'
-}
-
 /**
- * Gets change message for date property.
+ * Gets the change list for a plant update.
  *
- * @param {*} propName The property name
- * @param {*} oldDate The previous value
- * @param {*} newDate The new value
- * @returns {string}
+ * @param {*} changes Changes made to the plant
+ * @param {*} oldPlant The old plant object
+ * @return {Array} The change list
  */
-const getDateChangeMsg = (propName, newDate) => {
-  if (!newDate) return
+export const getChangeList = ({ $set }, oldPlant) => {
+  const changeList = []
 
-  const newDateObj = new Date(newDate)
-  return `${propName} changed to ${dayjs(newDateObj).format('YYYY-MM-DD')}`
+  // Walk thorough list of properties being set
+  for (const prop in $set) {
+    // Verify that the property exists
+    if (Object.hasOwn($set, prop)) {
+      // Get the change message for the property
+      const msg = getPropChangeMsg(prop, oldPlant[prop], $set[prop])
+      // Add message to array if not null
+      if (msg != null) changeList.push(msg)
+    }
+  }
+
+  return changeList
 }
 
 /**
@@ -69,27 +64,39 @@ const getPropChangeMsg = (propName, oldProp, newProp) => {
 }
 
 /**
- * Gets the change list for a plant update
+ * Gets status change message for a plant update.
  *
- * @param {*} changes Changes made to the plant
- * @param {*} oldPlant The old plant object
- * @return {Array} The change list
+ * @param {*} oldStatus
+ * @param {*} newStatus
+ * @returns
  */
-export const getChangeList = ({ $set }, oldPlant) => {
-  const changeList = []
+const getStatusChangeMsg = (oldStatus, newStatus) => {
+  // Status did not change
+  if (oldStatus === newStatus) return null
 
-  // Walk thorough list of properties being set
-  for (const prop in $set) {
-    // Verify that the property exists
-    if (Object.hasOwn($set, prop)) {
-      // Get the change message for the property
-      const msg = getPropChangeMsg(prop, oldPlant[prop], $set[prop])
-      // Add message to array if not null
-      if (msg != null) changeList.push(msg)
-    }
-  }
+  // Status is archived
+  if (newStatus === 'archived') return 'Plant archived.'
 
-  return changeList
+  // Status was archived and now is active
+  if (oldStatus === 'archived') return 'Plant unarchived.'
+
+  // Status was inactive and now is active
+  if (oldStatus === 'inactive') return 'Plant undeleted.'
+}
+
+/**
+ * Gets change message for date property.
+ *
+ * @param {*} propName The property name
+ * @param {*} oldDate The previous value
+ * @param {*} newDate The new value
+ * @returns {string}
+ */
+const getDateChangeMsg = (propName, newDate) => {
+  if (!newDate) return
+
+  const newDateObj = new Date(newDate)
+  return `${propName} changed to ${dayjs(newDateObj).format('YYYY-MM-DD')}`
 }
 
 export default { getChangeList }
