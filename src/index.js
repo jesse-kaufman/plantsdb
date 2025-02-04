bfaf46ce65b046ea3e4838bb7764acf8a2ac8e24
jesse-kaufman <jesse@jessekaufman.com>
2025-02-04 14:13:31 -0600

    Clear out file for refactor

--- src/index.js
+++ src/index.js
@@ -1,48 +1,5 @@
-// Server
-import compression from 'compression'
-import config from './config/config.js'
-import connectDB from './config/db.js'
-import cors from 'cors'
-import errorHandler from 'errorhandler'
-import express from 'express'
-import logRoutes from './routes/logRoutes.js'
-import plantRoutes from './routes/plantRoutes.js'
+import Plant from "./plants/classes/Plant.js"
 
-// MongoDB connection
-connectDB()
+const plant = new Plant({ name: "MyPlant" })
 
-// Setup Express
-const app = express()
-
-// Enable compression
-app.use(compression())
-
-// Interpret responses as JSON
-app.use(express.json())
-
-// Only parse URL-encoded bodies
-app.use(express.urlencoded({ extended: true }))
-
-// Use CORS
-app.use(cors())
-
-// Setup Express error handlers for dev environment
-if (process.env.NODE_ENV === 'development') {
-  app.use(errorHandler({ dumpExceptions: true, showStack: true }))
-}
-
-// Setup Express error handlers for production environment
-if (process.env.NODE_ENV === 'production') {
-  app.use(errorHandler())
-}
-
-// Plant routes
-app.use('/api/v1/plants', plantRoutes)
-
-// Log routes
-app.use('/api/v1/logs', logRoutes)
-
-// Start the server
-const server = app.listen(config.apiPort, () => {
-  console.log(`Express running → PORT ${server.address().port}`)
-})
+console.log(plant.name)
