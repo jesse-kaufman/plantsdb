65ecad6b5e3127bdd643798a36e613d6da064a57
jesse-kaufman <jesse@jessekaufman.com>
2025-02-04 16:49:32 -0600

    Add logic and tests for plant stages

--- src/__tests__/plants/Plant.test.js
+++ src/__tests__/plants/Plant.test.js
@@ -39,3 +39,29 @@ describe("Plant Name", () => {
     expect(plant.name).toBe("Alice")
   })
 })
+
+describe("Plant Stage", () => {
+  test("should throw an error when stage sent to constructor is invalid", () => {
+    // @ts-expect-error
+    expect(() => new Plant({ name: "Bob", stage: 1 })).toThrow("Invalid stage")
+  })
+  test("should throw an error when setting stage to invalid value", () => {
+    const plant = new Plant({ name: "Bob" })
+    // @ts-expect-error
+    expect(() => (plant.stage = undefined)).toThrow("Stage is required")
+    expect(() => (plant.stage = "left")).toThrow("Invalid stage")
+  })
+  test("should default to seedling when stage not provided to constructor", () => {
+    const plant = new Plant({ name: "Bob" })
+    expect(plant.stage).toEqual("seedling")
+  })
+  test("should set the stage correctly if stage is provided to constructor", () => {
+    const plant = new Plant({ name: "Bob", stage: "veg" })
+    expect(plant.stage).toBe("veg")
+  })
+  test("should set the stage correctly", () => {
+    const plant = new Plant({ name: "Bob", stage: "veg" })
+    plant.stage = "flower"
+    expect(plant.stage).toBe("flower")
+  })
+})
--- src/plants/Plant.js
+++ src/plants/Plant.js
@@ -11,17 +11,21 @@ export default class Plant {
 
   /** Name of plant. */
   #name
+  /** Plant stage */
+  #stage
 
   /**
    * Creates an instance of a Plant.
    *
    * @param {object} newPlant - Plant data to initialize the instance.
-   * @param {string} [newPlant.name=""] - Optional name of the plant (defaults to empty string).
+   * @param {string} newPlant.name - Name of the plant.
+   * @param {string} [newPlant.stage=""] - Optional stage of the plant (defaults to seedling).
    * @throws {Error} If the provided plant object fails validation.
    */
   constructor(newPlant) {
-    this.#validatePlant(newPlant)
-    this.#name = newPlant.name || "plant"
+    this.#validatePlant(newPlant, true)
+    this.#name = newPlant.name
+    this.#stage = newPlant.stage || "seedling"
   }
 
   /**
@@ -45,18 +49,41 @@ export default class Plant {
   }
 
   /**
+   * Gets the stage of the plant.
+   *
+   * @returns {string} Stage of the plant.
+   */
+  get stage() {
+    return this.#stage
+  }
+
+  /**
+   * Sets the stage of the plant.
+   *
+   * @param {string} newStage - The new stage of the plant.
+   * @throws {Error} If the new name is invalid.
+   */
+  set stage(newStage) {
+    this.#validateStage(newStage)
+    this.#stage = newStage
+  }
+
+  /**
    * Validates the given plant object.
    *
    * @param {object} plant - Plant data to initialize the instance.
-   * @param {string} [plant.name=""] - Name of the plant (optional).
+   * @param {string} plant.name - Name of the plant.
+   * @param {string} [plant.stage] - Name of the stage of the plant (optional only when creating instance)
+   * @param {boolean} isNew - Whether the plant is a new object or not.
    * @throws {Error} If plant object is invalid or any properties fail validation.
    */
-  #validatePlant(plant) {
+  #validatePlant(plant, isNew = false) {
     if (typeof plant !== "object" || plant === null) {
       throw new Error("Invalid plant object")
     }
 
     this.#validateName(plant.name)
+    this.#validateStage(plant.stage, !isNew)
   }
 
   /**
@@ -69,5 +96,24 @@ export default class Plant {
     if (name === undefined) throw new Error("Name is required")
     if (typeof name !== "string") throw new Error("Invalid name")
     if (name.trim() === "") throw new Error("Name is required")
+  }
+
+  /**
+   * Validates the provided stage.
+   *
+   * @param {string|undefined} stage - Stage to validate.
+   * @param {boolean} required - Whether to allow undefined for the stage.
+   * @throws {Error} If the stage is not a valid stage.
+   */
+  #validateStage(stage, required = true) {
+    // Stage is not required and is undefined
+    if (!required && stage === undefined) return
+
+    // Stage is required but undefined
+    if (required && stage === undefined) throw new Error("Stage is required")
+
+    // Stage is set and not a valid stage
+    if (!Plant.validStages.includes(stage?.toString() || ""))
+      throw new Error("Invalid stage")
   }
 }
