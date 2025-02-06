bf629e70cc8b2b2f6c5067bd866390b4ec320933
jesse-kaufman <jesse@jessekaufman.com>
2025-02-05 18:55:10 -0600

    Replace toThrowError() with toThrow() for consistency

--- __tests__/plants/unit/PlantName.test.js
+++ __tests__/plants/unit/PlantName.test.js
@@ -18,36 +18,32 @@ describe("Plant name property", () => {
 
   test("should throw TypeError when provided name is not string", () => {
     // Test sending non-string value to constructor
-    // @ts-expect-error
-    expect(() => new Plant({ name: true })).toThrowError(
+    expect(() => new Plant({ name: true })).toThrow(
       new TypeError("Name must be a string")
     )
-    // @ts-expect-error
-    expect(() => new Plant({ name: 123 })).toThrowError(
+    expect(() => new Plant({ name: 123 })).toThrow(
       new TypeError("Name must be a string")
     )
-    // @ts-expect-error
-    expect(() => new Plant({ name: null })).toThrowError(
+    expect(() => new Plant({ name: null })).toThrow(
       new TypeError("Name must be a string")
     )
 
     // Test setting name to non-string value
     const plant = new Plant({ name: "Bob" })
-    expect(() => (plant.name = true)).toThrowError(
+    expect(() => (plant.name = true)).toThrow(
       new TypeError("Name must be a string")
     )
-    expect(() => (plant.name = 123)).toThrowError(
+    expect(() => (plant.name = 123)).toThrow(
       new TypeError("Name must be a string")
     )
-    expect(() => (plant.name = null)).toThrowError(
+    expect(() => (plant.name = null)).toThrow(
       new TypeError("Name must be a string")
     )
   })
 
   test("should throw an error when setting name to undefined", () => {
     const plant = new Plant({ name: "Bob" })
-    // @ts-expect-error
-    expect(() => (plant.name = undefined)).toThrowError(
+    expect(() => (plant.name = undefined)).toThrow(
       new TypeError("Name is required")
     )
   })
