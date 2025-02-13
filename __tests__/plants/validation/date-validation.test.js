5de47402c24dd960786464549454b9bdf79db15e
jesse-kaufman <jesse@jessekaufman.com>
2025-02-13 11:13:32 -0600

    Add sample code for validation test for later

--- __tests__/plants/validation/date-validation.test.js
+++ __tests__/plants/validation/date-validation.test.js
@@ -6,3 +6,15 @@ describe("Plant - Date validation", () => {
     expect(true).toBe(true)
   })
 })
+
+/*
+ * Test("should throw an error if archivedOn is set and status isn't `archived`", () => {
+ *   expect(() => {
+ *     const plant = new Plant({
+ *       name: "Bob",
+ *       status: "active",
+ *       archivedOn: "2023-01-01",
+ *     })
+ *   }).toThrow(Error)
+ * })
+ */
