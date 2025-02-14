36e362370f9836c20fe35ffcffd94c6e8a9d05e7
jesse-kaufman <jesse@jessekaufman.com>
2025-02-14 13:12:02 -0600

    Add constants for valid stages/statuses

---
+++ src/plants/config/constants.js
@@ -0,0 +1,9 @@
+/**
+ * @file Constants used throughout the Plant class.
+ */
+
+/** List of valid stages. */
+export const validStages = ["seedling", "veg", "flower", "harvested", "cure"]
+
+/** List of valid plant statuses. */
+export const validStatuses = ["active", "inactive", "archived"]
