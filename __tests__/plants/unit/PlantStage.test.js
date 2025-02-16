13a9e0af877a6c1097fc567b57d1404b4b8a6840
jesse-kaufman <jesse@jessekaufman.com>
2025-02-16 17:59:38 -0600

    Set all properties when sending data to constructor

--- __tests__/plants/unit/PlantStage.test.js
+++ __tests__/plants/unit/PlantStage.test.js
@@ -5,16 +5,27 @@ import Plant from "../../../src/plants/Plant"
 
 describe("Plant - Stage property", () => {
   test("should throw an error when stage sent to constructor is invalid", () => {
-    expect(() => new Plant({ name: "Bob", stage: "" })).toThrow(
-      "Unknown plant stage: "
-    )
+    expect(
+      () =>
+        new Plant({
+          name: "Bob",
+          status: "active",
+          stage: "",
+          startedOn: "2023-01-01",
+        })
+    ).toThrow("Unknown plant stage: ")
     expect(() => new Plant({ name: "Bob", stage: true })).toThrow(
       "Invalid stage"
     )
   })
 
   test("should throw an error when setting stage to invalid value", () => {
-    const plant = new Plant({ name: "Bob" })
+    const plant = new Plant({
+      name: "Bob",
+      status: "active",
+      stage: "seedling",
+      startedOn: "2023-01-01",
+    })
     expect(() => (plant.stage = undefined)).toThrow("Stage is required")
     expect(() => (plant.stage = "")).toThrow("Unknown plant stage: ")
     expect(() => (plant.stage = "left")).toThrow("Unknown plant stage: left")
@@ -22,17 +33,33 @@ describe("Plant - Stage property", () => {
   })
 
   test("should default to seedling when stage not provided to constructor", () => {
-    const plant = new Plant({ name: "Bob" })
+    const plant = new Plant({
+      name: "Bob",
+      status: "active",
+      stage: "seedling",
+    })
     expect(plant.stage).toEqual("seedling")
   })
 
-  test("should set the stage correctly if stage is provided to constructor", () => {
-    const plant = new Plant({ name: "Bob", stage: "veg" })
+  test("should set the stage correctly when provided to constructor", () => {
+    const plant = new Plant({
+      name: "Bob",
+      status: "active",
+      startedOn: "2023-01-01",
+      vegStartedOn: "2023-01-08",
+      stage: "veg",
+    })
     expect(plant.stage).toBe("veg")
   })
 
   test("should set the stage correctly", () => {
-    const plant = new Plant({ name: "Bob", stage: "veg" })
+    const plant = new Plant({
+      name: "Bob",
+      status: "active",
+      startedOn: "2023-01-01",
+      vegStartedOn: "2023-01-08",
+      stage: "veg",
+    })
     plant.stage = "flower"
     expect(plant.stage).toBe("flower")
   })
