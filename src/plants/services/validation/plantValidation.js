fe3e45de6ea0a84ad5ecd0e6c4bac73e5d989fec
jesse-kaufman <jesse@jessekaufman.com>
2025-02-17 19:23:51 -0600

    Fix error thrown to match tests and remain consistent

--- src/plants/services/validation/plantValidation.js
+++ src/plants/services/validation/plantValidation.js
@@ -14,11 +14,11 @@ import { validateDate } from "./dateValidation"
  */
 export const validateName = (name) => {
   // Require a name
-  if (name === undefined) throw new Error("Name is required")
+  if (name === undefined) throw new Error("name is required")
   // Require name to be string
   if (typeof name !== "string") throw new TypeError("Name must be a string")
   // Require name to be non-empty and not only whitespace
-  if (name.trim() === "") throw new Error("Name is required")
+  if (name.trim() === "") throw new Error("name is required")
   // Require name to be at least 2 characters
   // eslint-disable-next-line no-magic-numbers
   if (name.trim().length <= 2) {
