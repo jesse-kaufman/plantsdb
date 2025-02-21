e112968058a742ade32596acd5c187508bef3c78
jesse-kaufman <jesse@jessekaufman.com>
2025-02-21 17:10:51 -0600

    fix: Update addWeeksToDate() definition to require date be a Date object

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -4,12 +4,11 @@
 
 /**
  * Adds given number of weeks to date and returns date object.
- * @param {Date|undefined} date - Date to be added to.
+ * @param {Date} date - Date to be added to.
  * @param {number} weeks - Number of weeks to add.
  * @returns {Date} New date after adding weeks.
  */
 export const addWeeksToDate = (date, weeks) => {
-  if (!(date instanceof Date)) throw new TypeError("Invalid date")
   if (isNaN(date.valueOf())) throw new Error("Invalid date")
 
   const newDate = new Date(date) // Create a copy of the original date
