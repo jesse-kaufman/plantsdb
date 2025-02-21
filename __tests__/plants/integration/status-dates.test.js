6d2f327b30445c33e96a1a951c136655feeee4e6
jesse-kaufman <jesse@jessekaufman.com>
2025-02-21 17:12:02 -0600

    test: Add check for archivedOn date when status is not archived

--- __tests__/plants/integration/status-dates.test.js
+++ __tests__/plants/integration/status-dates.test.js
@@ -8,14 +8,14 @@ describe("Plant - Status dates", () => {
   test("true should be true", () => {
     expect(true).toBe(true)
   })
-  // test("should throw an error if archivedOn is set and status isn't `archived`", () => {
-  //   expect(
-  //     () =>
-  //       new Plant({
-  //         name: "Bob",
-  //         status: "active",
-  //         archivedOn: "2023-01-01",
-  //       })
-  //   ).toThrow(Error)
-  // })
+  test("should throw an error if archivedOn is set and status isn't `archived`", () => {
+    expect(
+      () =>
+        new Plant({
+          name: "Bob",
+          status: "active",
+          archivedOn: "2023-01-01",
+        })
+    ).toThrow(Error)
+  })
 })
