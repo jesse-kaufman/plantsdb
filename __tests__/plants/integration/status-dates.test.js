8f114821b349c3bc3bf662c04e8ed0ef9a2429ce
jesse-kaufman <jesse@jessekaufman.com>
2025-02-21 19:19:15 -0600

    feat(test): Implement status / date property congruency tests

--- __tests__/plants/integration/status-dates.test.js
+++ __tests__/plants/integration/status-dates.test.js
@@ -4,18 +4,64 @@
 
 import Plant from "../../../src/plants/Plant"
 
-describe("Plant - Status dates", () => {
-  test("true should be true", () => {
-    expect(true).toBe(true)
+import { validArchivedPlant, validDeletedPlant } from "../testConstants"
+
+// Test archivedOn and deletedOn when status is archived
+describe("Archived status / date property congruency", () => {
+  // Check active plant with archivedOn set
+  it("should throw an error if archivedOn is set and status isn't 'archived'", () => {
+    // Get valid archived plant object and set status to active
+    const plant = { ...validArchivedPlant, status: "active" }
+    expect(() => new Plant(plant)).toThrow(
+      "archivedOn must be null for status active"
+    )
   })
-  test("should throw an error if archivedOn is set and status isn't `archived`", () => {
-    expect(
-      () =>
-        new Plant({
-          name: "Bob",
-          status: "active",
-          archivedOn: "2023-01-01",
-        })
-    ).toThrow(Error)
+
+  // Check archived plant with archivedOn = null
+  it("should throw an error if status is 'archived' and archivedOn is not set", () => {
+    // Get valid archived plant object and set archivedOn to null
+    const plant = { ...validArchivedPlant, archivedOn: null }
+    expect(() => new Plant(plant)).toThrow(
+      "archivedOn is required for status archived"
+    )
+  })
+
+  // Check archived plant with deletedOn set
+  it("should throw an error if status is 'archived' and deletedOn is set", () => {
+    // Get valid archived plant object and set archivedOn to null
+    const plant = { ...validArchivedPlant, deletedOn: "2023-01-01" }
+    expect(() => new Plant(plant)).toThrow(
+      "deletedOn must be null for status archived"
+    )
+  })
+})
+
+// Test deletedOn and archivedOn when status is deleted
+describe("Deleted status / date property congruency", () => {
+  // Check active plant with deletedOn set
+  it("should throw an error if deletedOn is set and status isn't 'deleted'", () => {
+    // Get valid deleted plant object and set status to active
+    const plant = { ...validDeletedPlant, status: "active" }
+    expect(() => new Plant(plant)).toThrow(
+      "deletedOn must be null for status active"
+    )
+  })
+
+  // Check deleted plant with deletedOn = null
+  it("should throw an error if status is 'deleted' and deletedOn is not set", () => {
+    // Get valid deleted plant object and set archivedOn to null
+    const plant = { ...validDeletedPlant, deletedOn: null }
+    expect(() => new Plant(plant)).toThrow(
+      "deletedOn is required for status inactive"
+    )
+  })
+
+  // Check deleted plant with archivedOn set
+  it("should throw an error if status is 'deleted' and archivedOn is set", () => {
+    // Get valid archived plant object and set archivedOn to null
+    const plant = { ...validArchivedPlant, deletedOn: "2023-01-01" }
+    expect(() => new Plant(plant)).toThrow(
+      "deletedOn must be null for status archived"
+    )
   })
 })
