60c7fd540e2fb6b6935644fbbfcc04fd7d642537
jesse-kaufman <jesse@jessekaufman.com>
2025-03-16 13:08:13 -0500

    chore: reorder file so order makes sense logically

--- src/plants/config/constants.js
+++ src/plants/config/constants.js
@@ -2,11 +2,10 @@
  * @file Constants used throughout the Plant class.
  */
 
-/** List of valid stages. */
-export const validStages = ["seedling", "veg", "flower", "harvested", "cure"]
-
 /** List of valid plant statuses. */
 export const validStatuses = ["active", "inactive", "archived"]
+/** List of valid stages. */
+export const validStages = ["seedling", "veg", "flower", "harvested", "cure"]
 /** Number of estimated weeks in seedling stages. */
 export const seedlingWeeks = 1
 /** Number of estimated weeks in veg stages. */
