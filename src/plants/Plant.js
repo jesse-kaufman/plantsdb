bc87052b79bec91fbba40ce9379ec119fefec228
jesse-kaufman <jesse@jessekaufman.com>
2025-03-16 11:24:30 -0500

    fix: allow some dates to be null

--- src/plants/Plant.js
+++ src/plants/Plant.js
@@ -17,13 +17,13 @@ import { seedlingWeeks, vegWeeks, flowerWeeks } from "./config/constants.js"
  * @property {string} status - Status of the plant being created.
  * @property {string} stage - Stage of the plant being created.
  * @property {string} startedOn - Date plant started.
- * @property {string} vegStartedOn - Date veg stage started.
- * @property {string} flowerStartedOn - Date flower stage started.
- * @property {string} potentialHarvest - Date of potential harvest (or null if harvested).
- * @property {string} harvestedOn - Date on which plant was harvested.
- * @property {string} cureStartedOn - Date on which plant started cure stage.
- * @property {string} archivedOn - Date plant was archived (or null if not archived).
- * @property {string} deletedOn - Date plant was deleted (or null if not deleted).
+ * @property {?string} vegStartedOn - Date veg stage started.
+ * @property {?string} flowerStartedOn - Date flower stage started.
+ * @property {?string} potentialHarvest - Date of potential harvest (or null if harvested).
+ * @property {?string} harvestedOn - Date on which plant was harvested.
+ * @property {?string} cureStartedOn - Date on which plant started cure stage.
+ * @property {?string} archivedOn - Date plant was archived (or null if not archived).
+ * @property {?string} deletedOn - Date plant was deleted (or null if not deleted).
  */
 
 /**
