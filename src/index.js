083384ff9ae041a9d3b350b104339eedfeb42ce3
jesse-kaufman <jesse@jessekaufman.com>
2025-03-16 11:20:34 -0500

    chore: add sample calls for basic testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,49 @@
 
 import Plant from "./plants/Plant.js"
 
-const plant = new Plant({ name: "MyPlant" })
+const newPlant = {
+  name: "Acapulco Gold",
+  status: "active",
+  stage: "seedling",
+  startedOn: "2023-01-01",
+  vegStartedOn: null,
+  flowerStartedOn: null,
+  potentialHarvest: null,
+  harvestedOn: null,
+  cureStartedOn: null,
+  archivedOn: null,
+  deletedOn: null,
+}
 
-console.log(plant.name)
+const plant = new Plant(newPlant)
+
+plant.print()
+
+plant.stage = "veg"
+plant.vegStartedOn = "2023-01-08"
+
+plant.print()
+
+plant.stage = "flower"
+plant.flowerStartedOn = "2024-02-08"
+
+plant.print()
+
+plant.stage = "harvested"
+plant.harvestedOn = "2024-11-08"
+
+plant.print()
+
+plant.stage = "cure"
+plant.cureStartedOn = "2024-11-15"
+
+plant.print()
+
+plant.delete()
+plant.print()
+
+plant.undelete()
+plant.print()
+
+plant.archive()
+plant.print()
