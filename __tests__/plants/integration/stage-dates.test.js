6bd02e83e241c169d20185e8d05a5da4fac10027
jesse-kaufman <jesse@jessekaufman.com>
2025-04-10 18:07:28 -0500

    test: wrap tests in  root level describe

--- __tests__/plants/integration/stage-dates.test.js
+++ __tests__/plants/integration/stage-dates.test.js
@@ -12,194 +12,196 @@ import {
 } from "../testConstants.js"
 import Plant from "@/plants/PlantClass.js"
 
-// Test dates for seedling stage
-describe("Seedling stage / date property congruency", () => {
-  // Check seedling plant with vegStartedOn set
-  it("should throw an error if vegStartedOn is set and stage is 'seedling'", () => {
-    // Get valid seedling plant object and set vegStartedOn
-    const plant = { ...validSeedlingPlant, vegStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "vegStartedOn must be null for seedling stage"
-    )
+describe("Stage dates", () => {
+  // Test dates for seedling stage
+  describe("Seedling stage / date property congruency", () => {
+    // Check seedling plant with vegStartedOn set
+    it("should throw an error if vegStartedOn is set and stage is 'seedling'", () => {
+      // Get valid seedling plant object and set vegStartedOn
+      const plant = { ...validSeedlingPlant, vegStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "vegStartedOn must be null for seedling stage"
+      )
+    })
+
+    // Check seedling plant with flowerStartedOn set
+    it("should throw an error if flowerStartOn is set and stage is 'seedling'", () => {
+      // Get valid seedling plant object and set flowerStartedOn
+      const plant = { ...validSeedlingPlant, flowerStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "flowerStartedOn must be null for seedling stage"
+      )
+    })
+
+    // Check seedling plant with harvestedOn set
+    it("should throw an error if harvestedOn is set and stage is 'seedling'", () => {
+      // Get valid seedling plant object and set harvestedOn
+      const plant = { ...validSeedlingPlant, harvestedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "harvestedOn must be null for seedling stage"
+      )
+    })
+
+    // Check seedling plant with cureStartedOn set
+    it("should throw an error if cureStartedOn is set and stage is 'seedling'", () => {
+      // Get valid seedling plant object and set cureStartedOn
+      const plant = { ...validSeedlingPlant, cureStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "cureStartedOn must be null for seedling stage"
+      )
+    })
   })
 
-  // Check seedling plant with flowerStartedOn set
-  it("should throw an error if flowerStartOn is set and stage is 'seedling'", () => {
-    // Get valid seedling plant object and set flowerStartedOn
-    const plant = { ...validSeedlingPlant, flowerStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "flowerStartedOn must be null for seedling stage"
-    )
+  // Test dates for veg stage
+  describe("Veg stage / date property congruency", () => {
+    // Check veg plant with vegStartedOn set to null
+    it("should throw an error if vegStartedOn is null and stage is 'veg'", () => {
+      // Get valid veg plant object and set vegStartedOn to null
+      const plant = { ...validVegPlant, vegStartedOn: null }
+      expect(() => new Plant(plant)).toThrow(
+        "vegStartedOn is required for veg stage"
+      )
+    })
+
+    // Check veg plant with flowerStartedOn set
+    it("should throw an error if flowerStartOn is set and stage is 'veg'", () => {
+      // Get valid veg plant object and set flowerStartedOn
+      const plant = { ...validVegPlant, flowerStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "flowerStartedOn must be null for veg stage"
+      )
+    })
+
+    // Check veg plant with harvestedOn set
+    it("should throw an error if harvestedOn is set and stage is 'veg'", () => {
+      // Get valid veg plant object and set harvestedOn
+      const plant = { ...validVegPlant, harvestedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "harvestedOn must be null for veg stage"
+      )
+    })
+
+    // Check veg plant with cureStartedOn set
+    it("should throw an error if cureStartedOn is set and stage is 'veg'", () => {
+      // Get valid veg plant object and set cureStartedOn
+      const plant = { ...validVegPlant, cureStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "cureStartedOn must be null for veg stage"
+      )
+    })
   })
 
-  // Check seedling plant with harvestedOn set
-  it("should throw an error if harvestedOn is set and stage is 'seedling'", () => {
-    // Get valid seedling plant object and set harvestedOn
-    const plant = { ...validSeedlingPlant, harvestedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "harvestedOn must be null for seedling stage"
-    )
+  // Test dates for flower stage
+  describe("Flower stage / date property congruency", () => {
+    // Check flower plant with flowerStartedOn set to null
+    it("should throw an error if flowerStartedOn is null and stage is 'veg'", () => {
+      // Get valid flower plant object and set flowerStartedOn to null
+      const plant = { ...validFlowerPlant, flowerStartedOn: null }
+      expect(() => new Plant(plant)).toThrow(
+        "flowerStartedOn is required for flower stage"
+      )
+    })
+
+    // Check flower plant with harvestedOn set
+    it("should throw an error if harvestedOn is set and stage is 'flower'", () => {
+      // Get valid flower plant object and set harvestedOn
+      const plant = { ...validFlowerPlant, harvestedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "harvestedOn must be null for flower stage"
+      )
+    })
+
+    // Check flower plant with cureStartedOn set
+    it("should throw an error if cureStartedOn is set and stage is 'flower'", () => {
+      // Get valid flower plant object and set cureStartedOn
+      const plant = { ...validFlowerPlant, cureStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "cureStartedOn must be null for flower stage"
+      )
+    })
   })
 
-  // Check seedling plant with cureStartedOn set
-  it("should throw an error if cureStartedOn is set and stage is 'seedling'", () => {
-    // Get valid seedling plant object and set cureStartedOn
-    const plant = { ...validSeedlingPlant, cureStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "cureStartedOn must be null for seedling stage"
-    )
-  })
-})
+  // Test dates for harvested stage
+  describe("Harvested stage / date property congruency", () => {
+    // Check harvested plant with harvestedOn set to null
+    it("should throw an error if harvestedOn is null and stage is 'veg'", () => {
+      // Get valid harvested plant object and set harvestedOn to null
+      const plant = { ...validHarvestedPlant, harvestedOn: null }
+      expect(() => new Plant(plant)).toThrow(
+        "harvestedOn is required for harvested stage"
+      )
+    })
 
-// Test dates for veg stage
-describe("Veg stage / date property congruency", () => {
-  // Check veg plant with vegStartedOn set to null
-  it("should throw an error if vegStartedOn is null and stage is 'veg'", () => {
-    // Get valid veg plant object and set vegStartedOn to null
-    const plant = { ...validVegPlant, vegStartedOn: null }
-    expect(() => new Plant(plant)).toThrow(
-      "vegStartedOn is required for veg stage"
-    )
-  })
-
-  // Check veg plant with flowerStartedOn set
-  it("should throw an error if flowerStartOn is set and stage is 'veg'", () => {
-    // Get valid veg plant object and set flowerStartedOn
-    const plant = { ...validVegPlant, flowerStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "flowerStartedOn must be null for veg stage"
-    )
+    // Check harvested plant with cureStartedOn set
+    it("should throw an error if cureStartedOn is set and stage is 'harvested'", () => {
+      // Get valid harvested plant object and set cureStartedOn
+      const plant = { ...validHarvestedPlant, cureStartedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "cureStartedOn must be null for harvested stage"
+      )
+    })
   })
 
-  // Check veg plant with harvestedOn set
-  it("should throw an error if harvestedOn is set and stage is 'veg'", () => {
-    // Get valid veg plant object and set harvestedOn
-    const plant = { ...validVegPlant, harvestedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "harvestedOn must be null for veg stage"
-    )
+  // Test dates for cure stage
+  describe("Cure stage / date property congruency", () => {
+    // Check cure plant with cureStartedOn set to null
+    it("should throw an error if cureStartedOn is null and stage is 'cure'", () => {
+      // Get valid cureStartedOn plant object and set cureStartedOn to null
+      const plant = { ...validCurePlant, cureStartedOn: null }
+      expect(() => new Plant(plant)).toThrow(
+        "cureStartedOn is required for cure stage"
+      )
+    })
   })
 
-  // Check veg plant with cureStartedOn set
-  it("should throw an error if cureStartedOn is set and stage is 'veg'", () => {
-    // Get valid veg plant object and set cureStartedOn
-    const plant = { ...validVegPlant, cureStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "cureStartedOn must be null for veg stage"
-    )
-  })
-})
+  describe("Stage date order", () => {
+    it("should throw an error if vegStartedOn is before startedOn", () => {
+      expect(
+        () =>
+          new Plant({
+            ...validVegPlant,
+            startedOn: "2023-02-01",
+            vegStartedOn: "2023-01-01",
+          })
+      ).toThrow("startedOn must come before vegStartedOn")
 
-// Test dates for flower stage
-describe("Flower stage / date property congruency", () => {
-  // Check flower plant with flowerStartedOn set to null
-  it("should throw an error if flowerStartedOn is null and stage is 'veg'", () => {
-    // Get valid flower plant object and set flowerStartedOn to null
-    const plant = { ...validFlowerPlant, flowerStartedOn: null }
-    expect(() => new Plant(plant)).toThrow(
-      "flowerStartedOn is required for flower stage"
-    )
-  })
+      const plant = new Plant({ ...validVegPlant, startedOn: "2023-01-01" })
+      expect(() => {
+        plant.vegStartedOn = "2022-01-01"
+      }).toThrow("startedOn must come before vegStartedOn")
+    })
 
-  // Check flower plant with harvestedOn set
-  it("should throw an error if harvestedOn is set and stage is 'flower'", () => {
-    // Get valid flower plant object and set harvestedOn
-    const plant = { ...validFlowerPlant, harvestedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "harvestedOn must be null for flower stage"
-    )
-  })
+    it("should throw an error if flowerStartedOn is before vegStartedOn", () => {
+      const plant = {
+        ...validFlowerPlant,
+        vegStartedOn: "2023-02-01",
+        flowerStartedOn: "2023-01-01",
+      }
+      expect(() => new Plant(plant)).toThrow(
+        "vegStartedOn must come before flowerStartedOn"
+      )
+    })
 
-  // Check flower plant with cureStartedOn set
-  it("should throw an error if cureStartedOn is set and stage is 'flower'", () => {
-    // Get valid flower plant object and set cureStartedOn
-    const plant = { ...validFlowerPlant, cureStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "cureStartedOn must be null for flower stage"
-    )
-  })
-})
+    it("should throw an error if harvestedOn is before flowerStartedOn", () => {
+      const plant = {
+        ...validHarvestedPlant,
+        flowerStartedOn: "2023-02-01",
+        harvestedOn: "2023-01-01",
+      }
+      expect(() => new Plant(plant)).toThrow(
+        "flowerStartedOn must come before harvestedOn"
+      )
+    })
 
-// Test dates for harvested stage
-describe("Harvested stage / date property congruency", () => {
-  // Check harvested plant with harvestedOn set to null
-  it("should throw an error if harvestedOn is null and stage is 'veg'", () => {
-    // Get valid harvested plant object and set harvestedOn to null
-    const plant = { ...validHarvestedPlant, harvestedOn: null }
-    expect(() => new Plant(plant)).toThrow(
-      "harvestedOn is required for harvested stage"
-    )
-  })
-
-  // Check harvested plant with cureStartedOn set
-  it("should throw an error if cureStartedOn is set and stage is 'harvested'", () => {
-    // Get valid harvested plant object and set cureStartedOn
-    const plant = { ...validHarvestedPlant, cureStartedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "cureStartedOn must be null for harvested stage"
-    )
-  })
-})
-
-// Test dates for cure stage
-describe("Cure stage / date property congruency", () => {
-  // Check cure plant with cureStartedOn set to null
-  it("should throw an error if cureStartedOn is null and stage is 'cure'", () => {
-    // Get valid cureStartedOn plant object and set cureStartedOn to null
-    const plant = { ...validCurePlant, cureStartedOn: null }
-    expect(() => new Plant(plant)).toThrow(
-      "cureStartedOn is required for cure stage"
-    )
-  })
-})
-
-describe("Stage date order", () => {
-  it("should throw an error if vegStartedOn is before startedOn", () => {
-    expect(
-      () =>
-        new Plant({
-          ...validVegPlant,
-          startedOn: "2023-02-01",
-          vegStartedOn: "2023-01-01",
-        })
-    ).toThrow("startedOn must come before vegStartedOn")
-
-    const plant = new Plant({ ...validVegPlant, startedOn: "2023-01-01" })
-    expect(() => {
-      plant.vegStartedOn = "2022-01-01"
-    }).toThrow("startedOn must come before vegStartedOn")
-  })
-
-  it("should throw an error if flowerStartedOn is before vegStartedOn", () => {
-    const plant = {
-      ...validFlowerPlant,
-      vegStartedOn: "2023-02-01",
-      flowerStartedOn: "2023-01-01",
-    }
-    expect(() => new Plant(plant)).toThrow(
-      "vegStartedOn must come before flowerStartedOn"
-    )
-  })
-
-  it("should throw an error if harvestedOn is before flowerStartedOn", () => {
-    const plant = {
-      ...validHarvestedPlant,
-      flowerStartedOn: "2023-02-01",
-      harvestedOn: "2023-01-01",
-    }
-    expect(() => new Plant(plant)).toThrow(
-      "flowerStartedOn must come before harvestedOn"
-    )
-  })
-
-  it("should throw an error if cureStartedOn is before harvestedOn", () => {
-    const plant = {
-      ...validCurePlant,
-      harvestedOn: "2024-02-01",
-      cureStartedOn: "2024-01-01",
-    }
-    expect(() => new Plant(plant)).toThrow(
-      "harvestedOn must come before cureStartedOn"
-    )
+    it("should throw an error if cureStartedOn is before harvestedOn", () => {
+      const plant = {
+        ...validCurePlant,
+        harvestedOn: "2024-02-01",
+        cureStartedOn: "2024-01-01",
+      }
+      expect(() => new Plant(plant)).toThrow(
+        "harvestedOn must come before cureStartedOn"
+      )
+    })
   })
 })
--- __tests__/plants/integration/status-dates.test.js
+++ __tests__/plants/integration/status-dates.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /**
  * @file Tests for Plant dates.
  */
@@ -6,62 +7,64 @@ import { describe, it, expect } from "vitest"
 import { validArchivedPlant, validDeletedPlant } from "../testConstants.js"
 import Plant from "@/plants/PlantClass.js"
 
-// Test archivedOn and deletedOn when status is archived
-describe("Archived status / date property congruency", () => {
-  // Check active plant with archivedOn set
-  it("should throw an error if archivedOn is set and status isn't 'archived'", () => {
-    // Get valid archived plant object and set status to active
-    const plant = { ...validArchivedPlant, status: "active" }
-    expect(() => new Plant(plant)).toThrow(
-      "archivedOn must be null for status active"
-    )
+describe("Status dates", () => {
+  // Test archivedOn and deletedOn when status is archived
+  describe("Archived status / date property congruency", () => {
+    // Check active plant with archivedOn set
+    it("should throw an error if archivedOn is set and status isn't 'archived'", () => {
+      // Get valid archived plant object and set status to active
+      const plant = { ...validArchivedPlant, status: "active" }
+      expect(() => new Plant(plant)).toThrow(
+        "archivedOn must be null for status active"
+      )
+    })
+
+    // Check archived plant with archivedOn = null
+    it("should throw an error if status is 'archived' and archivedOn is not set", () => {
+      // Get valid archived plant object and set archivedOn to null
+      const plant = { ...validArchivedPlant, archivedOn: null }
+      expect(() => new Plant(plant)).toThrow(
+        "archivedOn is required for status archived"
+      )
+    })
+
+    // Check archived plant with deletedOn set
+    it("should throw an error if status is 'archived' and deletedOn is set", () => {
+      // Get valid archived plant object and set archivedOn to null
+      const plant = { ...validArchivedPlant, deletedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "deletedOn must be null for status archived"
+      )
+    })
   })
 
-  // Check archived plant with archivedOn = null
-  it("should throw an error if status is 'archived' and archivedOn is not set", () => {
-    // Get valid archived plant object and set archivedOn to null
-    const plant = { ...validArchivedPlant, archivedOn: null }
-    expect(() => new Plant(plant)).toThrow(
-      "archivedOn is required for status archived"
-    )
-  })
+  // Test deletedOn and archivedOn when status is deleted
+  describe("Deleted status / date property congruency", () => {
+    // Check active plant with deletedOn set
+    it("should throw an error if deletedOn is set and status isn't 'deleted'", () => {
+      // Get valid deleted plant object and set status to active
+      const plant = { ...validDeletedPlant, status: "active" }
+      expect(() => new Plant(plant)).toThrow(
+        "deletedOn must be null for status active"
+      )
+    })
 
-  // Check archived plant with deletedOn set
-  it("should throw an error if status is 'archived' and deletedOn is set", () => {
-    // Get valid archived plant object and set archivedOn to null
-    const plant = { ...validArchivedPlant, deletedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "deletedOn must be null for status archived"
-    )
-  })
-})
+    // Check deleted plant with deletedOn = null
+    it("should throw an error if status is 'deleted' and deletedOn is not set", () => {
+      // Get valid deleted plant object and set archivedOn to null
+      const plant = { ...validDeletedPlant, deletedOn: null }
+      expect(() => new Plant(plant)).toThrow(
+        "deletedOn is required for status inactive"
+      )
+    })
 
-// Test deletedOn and archivedOn when status is deleted
-describe("Deleted status / date property congruency", () => {
-  // Check active plant with deletedOn set
-  it("should throw an error if deletedOn is set and status isn't 'deleted'", () => {
-    // Get valid deleted plant object and set status to active
-    const plant = { ...validDeletedPlant, status: "active" }
-    expect(() => new Plant(plant)).toThrow(
-      "deletedOn must be null for status active"
-    )
-  })
-
-  // Check deleted plant with deletedOn = null
-  it("should throw an error if status is 'deleted' and deletedOn is not set", () => {
-    // Get valid deleted plant object and set archivedOn to null
-    const plant = { ...validDeletedPlant, deletedOn: null }
-    expect(() => new Plant(plant)).toThrow(
-      "deletedOn is required for status inactive"
-    )
-  })
-
-  // Check deleted plant with archivedOn set
-  it("should throw an error if status is 'deleted' and archivedOn is set", () => {
-    // Get valid archived plant object and set archivedOn to null
-    const plant = { ...validArchivedPlant, deletedOn: "2023-01-01" }
-    expect(() => new Plant(plant)).toThrow(
-      "deletedOn must be null for status archived"
-    )
+    // Check deleted plant with archivedOn set
+    it("should throw an error if status is 'deleted' and archivedOn is set", () => {
+      // Get valid archived plant object and set archivedOn to null
+      const plant = { ...validArchivedPlant, deletedOn: "2023-01-01" }
+      expect(() => new Plant(plant)).toThrow(
+        "deletedOn must be null for status archived"
+      )
+    })
   })
 })
